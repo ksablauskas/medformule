809ff918482bcf921a00e17bd6a0981882c6be04
karsab <k.sablauskas@gmail.com>
2020-02-28 20:23:20 +0100

    covid hospital

--- src/views/About.vue
+++ src/views/About.vue
@@ -7,7 +7,7 @@
     
     <a href="https://bit.ly/39d978Y">Respulikinės Šiaulių ligoninės specialistų parengtus algoritmus</a>
     <v-container>
-      <iframe src="https://drive.google.com/file/d/1Np-L73a3Rpl4tLcL3Bn_RJ8gcHhaSMYJ/preview" width="1024" height="600"></iframe>
+      <iframe src="https://drive.google.com/file/d/1Np-L73a3Rpl4tLcL3Bn_RJ8gcHhaSMYJ/preview" width="100%" height="480px"></iframe>
     </v-container>
     
   </v-container>
