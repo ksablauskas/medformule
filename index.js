f1d4ac5879ca940dfd3565b3b7b767f82eea2385
karsab <k.sablauskas@gmail.com>
2020-11-01 15:01:51 +0200

    node conditional

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let path = require('path')
 let express = require('express')
 let port = process.env.PORT || 3000;
 app = express();
-app.use(servestatic(path.join(path.resolve(), 'dist')));
-app.listen(port, () => {
-    console.log("API server started on "+app.get('port'));});
+if(process.env.NODE_ENV === 'production'){
+    app.use(servestatic(path.join(path.resolve(), 'dist')));
+}
+app.listen(port, () => {console.log("API server started on "+app.get('port'));});
