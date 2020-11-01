9c94ace135972e8063d3c79e48b9fbd67c7bc232
karsab <k.sablauskas@gmail.com>
2020-11-01 15:23:14 +0200

    router

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ let servestatic = require('serve-static')
 let path = require('path')
 let express = require('express')
 let port = process.env.PORT || 3000;
-app = express();
+let app = express();
 if(process.env.NODE_ENV === 'production'){
     app.use(servestatic(path.join(path.resolve(), 'dist')));
 }
 app.listen(port, () => {console.log("API server started on "+app.get('port'));});
+
+let router = express.Router();
+router.get("/users", ...);
+app.use("/api", router);
+router.use((req, res, next) => {
+    res.setHeader('Content-Type', 'application/json');
+    next();
+}   
